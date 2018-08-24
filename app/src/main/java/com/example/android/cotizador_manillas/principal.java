57d3200f72189026bc2556d48b4b984c9cd38c5b
EnriquePallares <eapallaress@gmail.com>
2018-08-23 21:04:46 -0500

    Cotizador solo parte gráfica

---
+++ app/src/main/java/com/example/android/cotizador_manillas/principal.java
@@ -0,0 +1,13 @@
+package com.example.android.cotizador_manillas;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class principal extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+    }
+}
